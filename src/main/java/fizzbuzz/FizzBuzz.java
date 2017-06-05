2e2b53d3c1872475c066bd35e24ab83317417b18
guidoux <pedroguidoux@gmail.com>
2017-06-05 17:50:09 -0300

    Prints FizzBuzz results from 1 to 100

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,18 @@
 package fizzbuzz;
 
 public class FizzBuzz {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.fizzBuzz();
+    }
 
     public FizzBuzz() {}
+
+    public void fizzBuzz() {
+        for(int i=1; i<=100; i++) {
+            System.out.println(doFizzBuzz(i));
+        }
+    }
 
     public String doFizzBuzz(int input) {
         String result = Integer.toString(input);
